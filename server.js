ced70dcf5a95c8d250bda4feed10b2e742cc5c61
Dima-McArrow <dsm.box@gmail.com>
2024-11-08 21:14:53 +0100

    Adjustment of the path

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'resume' directory
-app.use(express.static(path.join(__dirname, "resume_scalingo_deploy")));
+// Serve static files from 'resume_scalingo_deploy'
+app.use(express.static(path.join(__dirname)));
+
+// Explicitly serve index.html at the root path
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
